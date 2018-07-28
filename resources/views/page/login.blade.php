d35c0fd83c2f10dc45c74026ee3401d94acdbff6
volervonwhittenburg <gradyalexis222@gmail.com>
2018-07-28 08:10:43 +0700

    login

---
+++ resources/views/page/login.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Login Form</title>
+    <link rel="STYLESHEET" href="<?php echo url(); ?>resources/assets/css/style.css" type="text/css" />
+</head>
+<body>
+    
+</body>
+</html>
