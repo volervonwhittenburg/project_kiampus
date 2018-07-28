2075255b5d5806b702abdcdf721f6105db11676e
volervonwhittenburg <gradyalexis222@gmail.com>
2018-07-28 08:37:30 +0700

    login masih error routes buat manggil login bisa dipindahin ke fungsi controller

--- resources/views/page/login.blade.php
+++ resources/views/page/login.blade.php
@@ -2,9 +2,16 @@
 <html>
 <head>
     <title>Login Form</title>
-    <link rel="STYLESHEET" href="<?php echo url(); ?>resources/assets/css/style.css" type="text/css" />
+    <link rel="STYLESHEET" href="<?php echo url()->full(); ?>/resources/assets/css/style.css" type="text/css" />
 </head>
-<body>
-    
+<body bgcolor="#00000f">
+    <table border="1">
+        <tr>
+            <td>
+            </td>
+        </tr>
+    </table>
+    <form action="" method="">
+    </form>
 </body>
 </html>
